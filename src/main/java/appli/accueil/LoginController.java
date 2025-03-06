1957fd8ab3b160d10ad29c61d890094748eafbe1
Romario QUASHIE <QUASHIE_Ro@lprs.local>
2025-03-06 16:24:03 +0100

    feat : Inscription

---
+++ src/main/java/appli/accueil/LoginController.java
@@ -0,0 +1,11 @@
+package appli.todolistfx.accueil;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module appli.todolistfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens appli to javafx.fxml;
+    exports appli;
+    exports appli.accueil;
+    opens appli.accueil to javafx.fxml;
+}
