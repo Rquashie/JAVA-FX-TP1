698363d0022a7684444baea5c7ad7579925b735d
Romario QUASHIE <QUASHIE_Ro@lprs.local>
2025-03-11 17:29:44 +0100

    feat : classe utilisateur

--- src/main/java/model/Utilisateur.java
+++ src/main/java/model/Utilisateur.java
@@ -2,26 +2,35 @@ package model;
 
 public class Utilisateur {
     private int id_utilisateur;
-    private String nom ;
-    private String prenom ;
-    private String email ;
-    private String password ;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String password;
 
-    public Utilisateur(int id_utilisateur , String nom , String prenom , String email , String password) {
-        this.id_utilisateur = id_utilisateur ;
+    public Utilisateur() {
+    }
+
+    public Utilisateur(String email , String password) {
+         this.email = email;
+         this.password = password;
+
+    }
+    public Utilisateur( String nom, String prenom, String email, String password) {
+
         this.nom = nom;
         this.prenom = prenom;
         this.email = email;
         this.password = password;
     }
-    public String getNom() {
-        return this.nom;
-    }
-    public String getPrenom() {
-        return this.prenom;
-    }
-    public String getEmail() {
-        return this.email;
-    }
 
+    public Utilisateur(int id_utilisateur, String nom, String prenom, String email, String password) {
+        this.id_utilisateur = id_utilisateur;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.password = password;
+        
+    }
 }
+
+
