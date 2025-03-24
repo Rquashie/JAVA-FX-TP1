654aae4ae59870fe69847a21e1fdae74abb44fab
rQuashie <kangoojuniors93@gmail.com>
2025-03-24 08:50:48 +0100

    feat : -ajout des boutons pour ajouter , modifier et supprimer une liste - Les boutons apparaissent lorsqu'on fait un double click

--- src/main/java/appli/accueil/AccueilController.java
+++ src/main/java/appli/accueil/AccueilController.java
@@ -24,56 +24,62 @@ import java.util.ResourceBundle;
 public class AccueilController implements Initializable {
 
 
-        @FXML
-        private Button ajouterListeButton;
-        @FXML
-        private TextField nomListeTextField;
-        @FXML
-        private Label erreurLabel;
-        @FXML
-        private TableView<Liste> tableView;
+@FXML
+private Button ajouterListeButton;
+@FXML
+private TextField nomListeTextField;
+@FXML
+private Label erreurLabel;
+@FXML
+private TableView<Liste> tableView;
+@FXML
+private Button ajouterTacheButton ;
+@FXML
+private Button modifierTacheButton ;
+@FXML
+private Button supprimerTacheButton ;
+@FXML
+private Label nomListeLabel ;
 
 
-        ListeRepository listeRepository = new ListeRepository();
 
-        public void ajouterListe(ActionEvent event) throws IOException, SQLException {
-                if (nomListeTextField.getText().isEmpty()) {
+ListeRepository listeRepository = new ListeRepository();
+
+
+
+
+public void ajouterListe(ActionEvent event) throws IOException, SQLException {
+    if (nomListeTextField.getText().isEmpty()) {
                         erreurLabel.setText("Veuillez entrer un nom");
-                } else if (listeRepository.getListe(nomListeTextField.getText())) {
-                        erreurLabel.setText("Veuillez entrer une liste différente");
-                } else {
-                        Liste liste = new Liste(nomListeTextField.getText());
-                        boolean ajout = listeRepository.ajouterListe(liste);
-                        if (ajout) {
-                                System.out.println("Liste ajouté");
-                        }
-                        if (liste.getId_liste() != 0) {
-                                System.out.println("Liste : " + liste.getId_liste());
-                                Utilisateur utilisateur = SessionUtilisateur.getInstance().getUtilisateur();
-
+    } else if (listeRepository.getListe(nomListeTextField.getText())) {
+        erreurLabel.setText("Veuillez entrer une liste différente");
+    } else {
+        Liste liste = new Liste(nomListeTextField.getText());
+        boolean ajout = listeRepository.ajouterListe(liste);
+        if (ajout) {
+            System.out.println("Liste ajouté");
+        }
+        if (liste.getId_liste() != 0) {
+            System.out.println("Liste : " + liste.getId_liste());
+            Utilisateur utilisateur = SessionUtilisateur.getInstance().getUtilisateur();
                         } else {
                                 System.out.println("ID inconnu");
                         }
                 }
         }
-
-        public void seDeconnecter(ActionEvent event) throws IOException {
+public void seDeconnecter(ActionEvent event) throws IOException {
                 SessionUtilisateur.getInstance().deconnecter();
                 StartApplication.changeScene("Connexion");
 
         }
-
-        public void actualiserListe(ActionEvent event) throws IOException {
+public void actualiserListe(ActionEvent event) throws IOException {
                 StartApplication.changeScene("Accueil");
         }
-
-
-        public void initialize(URL location, ResourceBundle resources) {
+public void initialize(URL location, ResourceBundle resources) {
                 String[][] colonnes = {
                         {"Id Liste", "id_liste"},
                         {"Nom", "nom"}
                 };
-
                 for (int i = 0; i < colonnes.length; i++) {
                         if (colonnes[i][0].equals("Id Liste")) {
                                 TableColumn<Liste, Integer> maCol = new TableColumn<>(colonnes[i][0]);
@@ -90,14 +96,26 @@ public class AccueilController implements Initializable {
                 for (Liste l : lesListes) {
                         tableView.getItems().add(l);
                 }
-        }
 
-        @FXML
-        public void gestionListe() throws IOException {
+    ajouterTacheButton.setVisible(false);
+    modifierTacheButton.setVisible(false);
+    supprimerTacheButton.setVisible(false);
+}
+@FXML
+public void gestionListe() throws IOException {
                 tableView.setOnMouseClicked(event -> {
                         if (event.getClickCount() == 2) {
                                 Liste listeSelectionnee = tableView.getSelectionModel().getSelectedItem();
                                 System.out.println("Id : " + listeSelectionnee.getId_liste() + "\nNom : " + listeSelectionnee.getNom());
+                                ajouterTacheButton.setVisible(true);
+                                modifierTacheButton.setVisible(true);
+                                supprimerTacheButton.setVisible(true);
+                                nomListeLabel.setVisible(false);
+                                nomListeLabel.setManaged(false);
+                                nomListeTextField.setVisible(false);
+                                ajouterListeButton.setVisible(false);
+
+
                         }
                 });
         }
